d5f7a7a29594d72055d76f849184f794dfafb9e6
imrehajnal <imre.hajnal19@gmail.com>
2024-03-13 09:29:06 +0000

    test

--- events.js
+++ events.js
@@ -1,6 +1,9 @@
 const buttonA = document.querySelector("#button-a");
 const buttonB = document.querySelector("#button-b");
 const result = document.querySelector("#result");
+const textA = document.querySelector("#text-a");
+const textB = document.querySelector("#text-b");
+const selectA = document.querySelector("#select-a");
 
 function print(message) {
   const ts = Date.now();
@@ -17,8 +20,23 @@ function handleButtonBClickEvent() {
   print("Button B Clicked");
 }
 
+function handleTextAClickEvent() {
+  print("Text A Clicked");
+}
+
+function handleTextBClickEvent() {
+  print("Text B Clicked");
+}
+
+function handleSelectAClickEvent() {
+  print("Select A Clicked");
+}
+
 buttonA.addEventListener("click", handleButtonAClickEvent);
 buttonB.addEventListener("click", handleButtonBClickEvent);
+textA.addEventListener("input", handleTextAClickEvent);
+textB.addEventListener("input", handleTextBClickEvent);
+
 
 window.addEventListener("load", () => {
   print("Window loaded");
